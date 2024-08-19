b84a5990f7a8726d6bddff74f197517099110bb1
MachadoLMX <lucas.mmds@hotmail.com>
2024-08-18 21:46:51 -0300

    Adicionado opção de saída
    Incluído no sistema a opção de saída.

--- Ebac_Cartorio_LucasMachado.c
+++ Ebac_Cartorio_LucasMachado.c
@@ -130,6 +130,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - Sair do Sistema.\n\n");
 		printf("Opção: "); //Fim do Menu
 			
 		scanf("%d", &opcao); // armazenando a escoha do usuário.
@@ -148,6 +149,11 @@ int main()
 
 			case 3:
 			deletar(); // Chamada de funções
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar nosso sistema!\n");
+			return 0;
 			break;
 			
 			default:
